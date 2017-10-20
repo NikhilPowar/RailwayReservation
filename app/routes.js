d2cf57486fed5f990b40d3900c647b21b343f8a2
Nikhil Powar <nikhilpowar16@gmail.com>
2017-10-20 14:09:50 +0530

    Adding new functions

--- app/routes.js
+++ app/routes.js
@@ -18,12 +18,23 @@ exports.route =  function(app){
     res.render('homepage.ejs');
   });
 
+  app.get('/search-trains', function(req, res){
+    res.render('search-trains.ejs');
+  });
+
+  app.get('/view-history', function(req, res){
+    res.render('view-history.ejs');
+  });
+
   app.post('/login-controller', function(req, res){
-      console.log("Email="+req.body.email);
       require('./../controllers/login-controller').login(req, res);
   });
 
   app.post('/signup-controller', function(req, res){
       require('./../controllers/signup-controller').register(req, res);
+  });
+
+  app.post('/search-controller', function(req, res){
+      require('./../controllers/search-controller').login(req, res);
   });
 }
