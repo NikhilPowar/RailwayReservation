0b47df63d54a3281197be4897e9a88ea6f47c902
HARSH KANSARA <harshkansara888@gmail.com>
2017-11-01 21:10:08 +0530

    Update remove_usercontroller.js

--- controllers/remove_usercontroller.js
+++ controllers/remove_usercontroller.js
@@ -1,5 +1,5 @@
 var connection = require('./../config');
 exports.remove_user=function(req.res){
   var email=req.body.email;
-  connection.query('DELETE FROM users where email='"+email+"'');
+  connection.query('DELETE FROM users WHERE email='"+email+"'');
 }
